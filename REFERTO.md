b6e83ab52ee96af963a6312b0171a35c77a7b985
unknown <shubhamgoel785@gmail.com>
2019-01-09 18:14:16 +0530

    first commit

---
+++ REFERTO.md
@@ -0,0 +1,22 @@
+# Learning objects
+
+## GIT
+* [How to convert existing non-empty directory into a Git working directory and push files to a remote repository](http://stackoverflow.com/questions/3311774/how-to-convert-existing-non-empty-directory-into-a-git-working-directory-and-pus):
+```
+cd <localdir>
+git init
+git add .
+git commit -m 'message'
+git remote add origin <url>
+git push -u origin master
+```
+* [Git flow cheatsheet](https://danielkummer.github.io/git-flow-cheatsheet/)
+
+* [Avoid prompt to input username and password](http://stackoverflow.com/questions/8588768/git-push-username-password-how-to-avoid)
+```
+git config credential.helper store
+git push https://github.com/YOUR-REPO.git
+
+Username for 'https://github.com': <USERNAME>
+Password for 'https://USERNAME@github.com': <PASSWORD>
+```
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::resource('category', 'CategoryController');
+
+	Route::get('post/trashed', 'PostController@trashed')->name('post.trashed');
+	Route::get('post/{id}/restore', 'PostController@restore')->name('post.restore');
+	Route::delete('post/{id}/delete', 'PostController@delete')->name('post.delete');
+
+	Route::resource('post', 'PostController');
+
+
+	Route::group(['prefix' => 'permissions'], function () {
+
+		Route::get('/', function (){
+			$roles = Spatie\Permission\Models\Role::all()->toArray();
+			$permissions = Spatie\Permission\Models\Permission::all()->toArray();
+			dd($roles, $permissions);
+		});
+
+		Route::get('{role}/users', function ($role){
+			$users = App\User::role('admin')->get()->toArray();
+			dd($role, $users);
+		});
+
+		Route::get('{role}/give', function ($role){
+			Auth::user()->assignRole($role);
+			return redirect('permissions/'.$role.'/users');
+		});
+
+		Route::get('create', function (){
+			$role = Spatie\Permission\Models\Role::create(['name' => 'customer', 'user']);
+			//$permission = Spatie\Permission\Models\Permission::create(['name' => 'system admin']);
+			return redirect('permissions');
+		});
+
+	});
+
+	Route::get('user/profile', function () {
+	// Uses Auth Middleware
+	});
+});
